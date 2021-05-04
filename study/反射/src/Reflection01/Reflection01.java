b40ea3b599006b0e75bfa959dd50245e7b2690f0
pywzzz <1021640832@qq.com>
2021-05-04 21:44:02 +0800

    在优化后可以看出来利用反射机制调用的执行速度会提升一些：
    一般方法耗时：3
    反射机制耗时：513
    优化后反射机制耗时：265

--- study/反射/src/Reflection01/Reflection01.java
+++ study/反射/src/Reflection01/Reflection01.java
@@ -6,6 +6,7 @@ public class Reflection01 {
     public static void main(String[] args) throws Exception {
         m1();
         m2();
+        m3();
     }
 
     // m1是一般方法调用
@@ -30,6 +31,20 @@ public class Reflection01 {
         }
         long end = System.currentTimeMillis();
         System.out.println("反射机制耗时：" + (end - start));
+    }
+
+    // m3是优化后的利用反射机制去调用
+    public static void m3() throws Exception {
+        Class cls = Class.forName("Reflection01.Cat");
+        Object o = cls.newInstance();
+        Method hi = cls.getMethod("hi");
+        hi.setAccessible(true); // 取消在利用反射机制去调用方法时所产生的访问检查从而提高效率
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 90000000; i++) {
+            hi.invoke(o);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("优化后反射机制耗时：" + (end - start));
     }
 
 }
